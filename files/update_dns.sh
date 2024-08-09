25c3468d727e775991396a210ecd7996d838f7df
jamesf <jamesf_coder@protonmail.com>
2024-08-09 11:17:42 +0100

    Added a better way of updating the Cloudflare DNS record

---
+++ files/update_dns.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#cloudflare_api_key=$1
+#domain_name=$2
+
+logger -p local7.info "Updating Cloudflare DNS"
+
+zone=$1
+dnsrecord=$1
+cloudflare_auth_email=$2
+cloudflare_auth_key=$3
+
+# call out to icanhazip.com to get the public ip address of the server
+ip=$(curl -s -X GET ipv4.icanhazip.com)
+
+if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+  echo "$dnsrecord is currently set to $ip; no changes needed"
+  exit
+fi
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "Zoneid for $zone is $zoneid"
+
+curl -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id'
+
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
+
+logger -p local7.info "Updated Cloudflare DNS to: $dnsrecord"
